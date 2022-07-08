ee726a3807cf68fc179828cea9ec29c772950e22
LoQeN00 <LoQeN00@o2.pl>
2022-07-08 14:30:13 +0200

    Test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 type Props = {};
 
 const HomePage = (props: Props) => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-2xl text-red-500">HomePage</h1>
+    </div>
+  );
 };
 
 export default HomePage;
