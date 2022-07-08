82bca5a59b71980f7efd3f925f450bb7e3285b5c
LoQeN00 <LoQeN00@o2.pl>
2022-07-08 14:38:09 +0200

    Redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { useSession, getSession } from 'next-auth/react';
+import { GetServerSidePropsContext } from 'next';
 
 type Props = {};
 
 const HomePage = (props: Props) => {
+  const { data: session, status } = useSession();
+
   return (
     <div>
       <h1 className="text-2xl text-red-500">HomePage</h1>
     </div>
   );
+};
+
+export const getServerSideProps = async ({ req, res }: GetServerSidePropsContext) => {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      props: {},
+      redirect: {
+        destination: '/api/auth/signin',
+        permament: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default HomePage;
